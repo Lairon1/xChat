7037fa1e9e2ceae34a030a1b495d95fd6475c56e
0xLairon1 <62989407+Lairon1@users.noreply.github.com>
2023-03-06 20:50:49 +0300

    init

---
+++ 0xChat/src/main/java/com/lairon/plugins/xchat/ChatPlugin.java
@@ -0,0 +1,17 @@
+package com.lairon.plugins.xchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
