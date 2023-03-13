06c5cc50508792f01c587efa6f67c066256d02ef
0xLairon1 <62989407+Lairon1@users.noreply.github.com>
2023-03-13 22:51:22 +0300

    removed src-delomboked

--- src-delomboked/main/java/com/lairon/plugins/xchat/config/LangConfig.java
+++
@@ -1,8 +0,0 @@
-package com.lairon.plugins.xchat.config;
-
-public interface LangConfig {
-
-    String chatNotFound();
-    String yourMessageIsEmpty();
-
-}
--- src-delomboked/main/java/com/lairon/plugins/xchat/handler/ChatHandler.java
+++
@@ -1,8 +0,0 @@
-package com.lairon.plugins.xchat.handler;
-
-import com.lairon.plugins.xchat.AbstractPlayer;
-import lombok.NonNull;
-
-public interface ChatHandler {
-    void handleChat(@NonNull AbstractPlayer player, @NonNull String message);
-}
